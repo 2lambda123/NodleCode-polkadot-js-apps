a85767ae58088dfeb1f0eed741d04bf8043b678d
douglasacost <douglasacostaaquino@gmail.com>
2023-01-09 13:19:14 -0400

    mode rule from top to middle

--- packages/apps-config/src/endpoints/nodle.ts
+++ packages/apps-config/src/endpoints/nodle.ts
@@ -1,7 +1,7 @@
-/* eslint-disable header/header */
 // Copyright 2017-2022 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable header/header */
 import type { TFunction } from '../types';
 import type { LinkOption } from './types';
 
