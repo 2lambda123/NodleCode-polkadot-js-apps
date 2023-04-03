ce0d37f86610a5cddb47303e203c26cf0d3bf4f3
douglasacost <douglasacostaaquino@gmail.com>
2023-04-03 18:20:25 +0200

    remove bad headers

--- packages/apps-config/src/endpoints/nodle.ts
+++ packages/apps-config/src/endpoints/nodle.ts
@@ -1,7 +1,6 @@
-// SPDX-License-Identifier: Apache-2.0
-
 // Copyright 2017-2022 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+
 /* eslint-disable header/header */
 import type { TFunction } from '../types.js';
 import type { LinkOption } from './types.js';
