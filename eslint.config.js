b78a5f9fcedcf8dcefb24d2bd080429d24d5feed
douglasacost <douglasacostaaquino@gmail.com>
2024-01-08 18:44:32 +0100

    Disable header/header rule in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,8 @@ export default [
       // add override for any (a metric ton of them, initial conversion)
       '@typescript-eslint/no-explicit-any': 'off',
       // we generally use this in isFunction, not via calling
-      '@typescript-eslint/unbound-method': 'off'
+      '@typescript-eslint/unbound-method': 'off',
+      'header/header': 'off'
     }
   }
 ];
